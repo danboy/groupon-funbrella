dd1dd4ae1fd4fc667b7b2b889570d98d06e22233
Dan Nawara <dnawara@groupon.com>
2012-05-29 13:45:33 -0500

    added a slideshow scriptw

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,17 +1,21 @@
 var Funbrella = Funbrella || {};
 
-Funbrella.list = function(list){
+Funbrella.list = function(list,speed){
   this.frame = 0;
-  this.max = list.length;
+  this.max = list.length-1;
   this.list = list;
   list.each(function(index, item){
     $(item).hide();
     console.log(item);
   });
+  setInterval(function(){this.animate()}.bind(this),speed);
 }
 
 Funbrella.list.prototype = {
   animate: function(item){
-    this.frame++;
+    $(this.list).hide();
+    $(this.list[this.frame]).show();
+    (this.frame === this.max) ? this.frame = 0 : this.frame++;
+    return this.frame;
   }
 };
